8b7fffdd4bb7bcd648ebfb7c07f68ade6ef8bf6a
nitesh22061997@gmail.com <163007323+Nitesh22061997@users.noreply.github.com>
2024-06-08 17:49:45 +0530

    new url

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
     useEffect(() => {
         if (authUser) {
-            const socket = io("https://chat-app-kcli.onrender.com", {
+            const socket = io("https://chat-app-yfe3.onrender.com", {
                 query: {
                     userId: authUser._id,
                 },
